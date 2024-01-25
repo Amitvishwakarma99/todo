363e44e6d48ce681f59b188bd3abb445f03fdc11
amit vishwakarma <amit1999vishwakarma@gmail.com>
2024-01-25 10:41:52 +0530

    solved phone compatibility with icons

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -32,7 +32,7 @@ const ToDoItem = ({todo}) => {
         onChange={(e) => setTodoMsg(e.target.value)}
         readOnly={!isTodoEditable}
       />
-      <div className="flex pr-2 ">
+      <div className="flex pr-2  ">
         <button
           className="disabled:opacity-50  w-12 px-2 "
           onClick={() => {
